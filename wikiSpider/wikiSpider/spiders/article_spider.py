cb84814cd991836e6ec34ed598aefaa101311e81
Ilshat Minnullin <minnullinilshat@gmail.com>
2022-09-19 16:57:20 +0300

    article_spider completed

---
+++ wikiSpider/wikiSpider/spiders/article_spider.py
@@ -0,0 +1,29 @@
+from scrapy.linkextractors import LinkExtractor 
+from scrapy.spiders import CrawlSpider, Rule 
+from wikiSpider.items import Article 
+
+class ArticleSpider(CrawlSpider):
+    name = 'article_items'
+    allowed_domains = ['wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/Game_of_Thrones']
+    rules = [
+        Rule(LinkExtractor(allow=r'^(/wiki/)((?!:).)*$'),
+            callback='parse_items', follow=True),
+        Rule(LinkExtractor(
+            allow=r'^(https://en.wikipedia.org/wiki/)((?!:).)*$'),
+            callback='parse_items', follow=True),
+    ]
+
+    def parse_items(self, response):
+        article = Article() 
+        article['url'] = response.url
+        article['title'] = response.xpath(
+            '//*[@id="mw-content-text"]/div[1]/p[2]/b[1]/text()').get()
+        article['text'] = response.xpath(
+            '//div[@id="mw-content-text"]/'
+            'div[@class="mw-parser-output"]/p/text()').getall()
+        last_updated = response.css(
+            'li#footer-info-lastmod::text').get()
+        article['last_updated'] = last_updated.replace(
+            'This page was last edited on ', '')
+        return article 
--- wikiSpider/wikiSpider/spiders/articles.py
+++ wikiSpider/wikiSpider/spiders/articles.py
@@ -10,17 +10,20 @@ class ArticleSpider(CrawlSpider):
         Rule(LinkExtractor(allow=r'^(/wiki/)((?!:).)*$'),
             callback='parse_items', follow=True,
             cb_kwargs={'is_article': True}),
+        Rule(LinkExtractor(allow=r'^(https://en.wikipedia.org/wiki/)((?!:).)*$'),
+            callback='parse_items', follow=True,
+            cb_kwargs={'is_article': True}),
         Rule(LinkExtractor(allow=r'.*'), callback='parse_items',
             cb_kwargs={'is_article': False})
     ]
 
     def parse_items(self, response, is_article):
         print(response.url)
-        title = response.css('h1::text').get()
+        title = response.xpath('//*[@id="mw-content-text"]/div[1]/p[2]/b[1]/text()').get()
         if is_article:
             url = response.url 
-            text = response.xpath('//div[@id="mw-content-text"]'
-                '//text()').get() 
+            text = response.xpath('//div[@id="mw-content-text"]/'
+                'div[@class="mw-parser-output"]/p/text()').getall()
             last_updated = response.css('li#footer-info-lastmod'
                 '::text').get() 
             last_updated = last_updated.replace('This page was '
